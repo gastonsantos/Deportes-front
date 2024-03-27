8beb2fe92b0933fb135912a39a4d4eea9409a809
Gaston Santos <gastons525@gmail.com>
2024-03-27 15:41:13 -0300

    dividi en componente PerfilesInfo y agregue page deportes que no se porq no estaba

--- app/pages/perfil/futbol/page.tsx
+++ app/pages/perfil/futbol/page.tsx
@@ -1,27 +1,16 @@
 'use client'
 import React, {useState, useEffect} from "react";
-import {obtenerFichaDeportistaPorId,actualizarFichaDeportistaPorId } from "@/services/perfiles/api";
 import AsideComponent from "@/components/navegation/AsideComponent";
 import { NavBar } from "@/components/navBar/navBar";
-
 import PerfilesStats from "@/components/perfiles/PerfilesStats";
-import Swal from 'sweetalert2';
+import PerfilesInfo  from "@/components/perfiles/PerfilesInfo";
 
 
 
 export default function PerfilFutbol()
 {
     
-  const [formData, setFormData] = useState({
-    idUsuario: localStorage.getItem("id"),
-    edad: '',
-    altura: '',
-    peso: '',
-    pieHabil: '',
-    manoHabil:'',
-    posicion: '',
-    
-  });
+
 
 
   const [openMenu, setOpenMenu] = useState(false)
@@ -29,60 +18,7 @@ export default function PerfilFutbol()
       
 //agregar Handle de perfil usuario deportista
 
-      useEffect(() => {
-        const fetchData = async () => {
-          try {
-            
-            const response = await obtenerFichaDeportistaPorId();
-            if (response) {
-              console.log("que trae la respuesta", response);
-              
-              const data = response
-              
-              setFormData(data); // Almacena los datos
-            }
-          } catch (error) {
-            console.error("Error al obtener deportes:", error);
-          }
-        };
-    
-        fetchData();
-      }, []);
-    
-      const handleChange = (e: any) => {
-        console.log(e.target, "que tiene e")
-        const { name, value } = e.target;
-        console.log("Name", name, value);
-
-        setFormData(prevFormData => ({
-          ...prevFormData,
-          [name]: value
-        }));
-        console.log("AVERR", formData);
-      };
-    
-      const handleSubmit = async () => {
-        try {
-          const response = await actualizarFichaDeportistaPorId(formData);
-          console.log("Que trae Response", response)
-          if (response) {
-            console.log('Ficha deportista actualizada:', response);
-            Swal.fire({
-              title: '¡Ficha deportista actualizada!',
-              text: 'Se ha modificado correctamente.',
-              icon: 'success',
-              confirmButtonText: 'Continuar',
-              confirmButtonColor: '#007bff', // Adjust color as needed
-            }).then(() => {
-              // Optional behavior after success (e.g., redirect to login)
-              
-            });
-          }
-        } catch (error) {
-          console.error('Error al actualizar la ficha deportista:', error);
-        }
-      };
-     
+  
 
 return(
     <>
@@ -114,75 +50,7 @@ return(
            
           </div>
           {/* Form Section */}
-          <form >
-            <div className="flex flex-col justify-center  md:grid md:grid-cols-2 gap-4">
-          
-              <div>
-                <label htmlFor="edad" className="block text-gray-700 text-sm font-bold mb-2 text-center md:text-left">Edad</label>
-                <input type="number" name="edad" id="edad" value={formData.edad} onChange={handleChange} className="w-full px-4 py-2 border rounded focus:outline-none focus:border-green-500 text-gray-700 border-blue-500" />
-              </div>
-              <div>
-                <label htmlFor="altura" className="block text-gray-700 text-sm font-bold mb-2 text-center md:text-left">Altura</label>
-                <input type="number" name="altura" id="altura" value={formData.altura} onChange={handleChange} className="w-full px-4 py-2 border rounded focus:outline-none focus:border-green-500 text-gray-700 border-blue-500" />
-              </div>
-              <div>
-                <label htmlFor="peso" className="block text-gray-700 text-sm font-bold mb-2 text-center md:text-left">Peso</label>
-                <input type="number" name="peso" id="peso" value={formData.peso} onChange={handleChange} className="w-full px-4 py-2 border rounded focus:outline-none focus:border-green-500 text-gray-700 border-blue-500" />
-              </div>
-              
-              
-              <div className="mb-6">
-              <label htmlFor="pieHabil" className="block text-gray-700 text-sm font-bold mb-2 text-center md:text-left">Pie habil</label>
-              <select
-               id="pieHabil"
-               value={formData.pieHabil}
-               name="pieHabil"
-                onChange={handleChange}
-               className="w-full px-4 py-2 border rounded focus:outline-none focus:border-green-500 text-gray-700 border-blue-500"
-               >
-              <option value="Derecho">Derecho</option>
-             <option value="Izquierdo">Izquierdo</option>
-            </select>
-            </div>
-            <div className="mb-6">
-              <label htmlFor="manoHabil" className="block text-gray-700 text-sm font-bold mb-2 text-center md:text-left">Mano habil</label>
-              <select
-               id="manoHabil"
-               name="manoHabil"
-               value={formData.manoHabil}
-                onChange={handleChange}
-               className="w-full px-4 py-2 border rounded focus:outline-none focus:border-green-500 text-gray-700 border-blue-500"
-               >
-              <option value="Derecho">Derecho</option>
-             <option value="Izquierdo">Izquierdo</option>
-            </select>
-            </div>
-            </div>
-            <div className="mb-6">
-              <label htmlFor="posicion" className="block text-gray-700 text-sm font-bold mb-2 text-center md:text-left">Posicion</label>
-              <select id="posicion"
-              name="posicion"
-               value={formData.posicion}
-                onChange={handleChange}
-               className="w-full px-4 py-2 border rounded focus:outline-none focus:border-green-500 text-gray-700 border-blue-500"
-               >
-              <option value="Arquero">Arquero</option>
-             <option value="Defensor">Defensor</option>
-             <option value="Mediocampista">Mediocampista</option>
-             <option value="Delantero">Delantero</option>
-            </select>
-            </div>
-            {/* Buttons */}
-            <div className="flex justify-center md:justify-end">
-              <button
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue"
-                type="button"
-                onClick={handleSubmit}
-              >
-                Guardar
-              </button>
-            </div>
-          </form>
+          <PerfilesInfo/>
         </div>
       {/*statsCards*/}
     <PerfilesStats/>
