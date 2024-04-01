53e6d1378b5549c39c80395dc34782c53273765c
osc123 <osc.eck@gmail.com>
2024-03-31 23:23:25 -0300

    cambios

--- app/pages/crearEvento/page.jsx
+++ app/pages/crearEvento/page.jsx
@@ -10,6 +10,10 @@ export default function Crear() {
         <div className="w-1/2 ">
         <h2>¿Qué vas a jugar?</h2>
         <Carousel/>
+        {/*agregar un aviso de que aun no se puede crear un deporte en caso de que no exista 
+        En segundo lugar agregar la opcion de crear evento con el forumario ya que al estar vacio, deberira decir algo como 
+        "Aun no hay eventos, porque no creas uno? o alguna frase con un boton al form */}
+
         </div>
         </div>
     </div>
