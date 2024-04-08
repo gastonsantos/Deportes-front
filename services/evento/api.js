721bd21572a7875f33e9fdb2f2ca7a89ae4bf068
Gaston Santos <gastons525@gmail.com>
2024-04-08 14:26:02 -0300

    no me acuerdo algo de evento

--- services/evento/api.js
+++ services/evento/api.js
@@ -6,10 +6,11 @@ import axiosInstance from '@/services/interceptor/api'
 const agregarEvento = async (data) =>  {
   try {
     const response = await axiosInstance.post('/evento/AgregarEvento', data);
-    console.log('agregarEvento', response);
+    console.log('agregarEvento', response.status);
+
     return response;
   } catch (error) {
-    throw error; // Re-throw error for handling in the calling component
+    throw error; 
   }
 }
 
