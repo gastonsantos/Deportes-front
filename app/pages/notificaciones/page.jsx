61c7ca1b7c04d2112d9f151a68e0427ac4a0086b
Gaston Santos <gastons525@gmail.com>
2024-04-26 12:48:41 -0300

    Mejore la vista notificaciones

--- app/pages/notificaciones/page.jsx
+++ app/pages/notificaciones/page.jsx
@@ -1,36 +1,36 @@
 "use client"
-import Link from 'next/link';
-import { NavBar } from "@/components/navBar/navBar";
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 import Footer from "@/components/landing/footer";
+import { NavBar } from "@/components/navBar/navBar";
+import { aceptarNotificacion, rechazarNotificacion, traerTodasLasNotificaciones } from "@/services/notificaciones/api";
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
-import { traerTodasLasNotificaciones, aceptarNotificacion, rechazarNotificacion } from "@/services/notificaciones/api"
 const Notificaciones = () => {
     const router = useRouter();
     const [notificaciones, setNotificaciones] = useState([]);
     const [cantidad, setCantidad] = useState();
 
 
-    
-        const fetchData = async () => {
-            try {
-                const response = await traerTodasLasNotificaciones();
-                if (response) {
-                    const data = response
-                    console.log("Notificaciones", data);
-                    setNotificaciones(data); 
-                }
-            } catch (error) {
-                console.error("Error al obtener las notificaciones:", error);
-            }
-        };
 
-    
+    const fetchData = async () => {
+        try {
+            const response = await traerTodasLasNotificaciones();
+            if (response) {
+                const data = response
+                console.log("Notificaciones", data);
+                setNotificaciones(data);
+            }
+        } catch (error) {
+            console.error("Error al obtener las notificaciones:", error);
+        }
+    };
+
+
     useEffect(() => {
         fetchData();
     }, []);
-  
+
     const handleAceptar = async (idParticipantes) => {
         const data = {
             idUsuario: idParticipantes
@@ -39,16 +39,16 @@ const Notificaciones = () => {
             const response = await aceptarNotificacion(data)
             if (response) {
                 Swal.fire({
-                  title: '¡Se acepto la notificación',
-                  text: 'Te has unido al evento!',
-                  icon: 'success',
-                  confirmButtonText: 'Continuar',
-                  confirmButtonColor: '#007bff', // Adjust color as needed
+                    title: '¡Se acepto la notificación',
+                    text: 'Te has unido al evento!',
+                    icon: 'success',
+                    confirmButtonText: 'Continuar',
+                    confirmButtonColor: '#007bff', // Adjust color as needed
                 }).then(() => {
                     fetchData();;
-        
+
                 });
-              }
+            }
         } catch (error) {
             Swal.fire({
                 title: 'No se pudo aceptar la notificación',
@@ -56,28 +56,28 @@ const Notificaciones = () => {
                 icon: 'error',
                 confirmButtonText: 'OK',
                 confirmButtonColor: '#007bff'
-              });
+            });
         }
     }
     const handleRechazar = async (idParticipantes) => {
         const data = {
             idUsuario: idParticipantes
         }
-      
+
         try {
             const response = await rechazarNotificacion(data)
             if (response) {
                 Swal.fire({
-                  title: '¡Se rechazó satisfactoriamente la invitación',
-                  text: 'Has rechazado el evento',
-                  icon: 'warning',
-                  confirmButtonText: 'Continuar',
-                  confirmButtonColor: '#007bff', // Adjust color as needed
+                    title: '¡Se rechazó satisfactoriamente la invitación',
+                    text: 'Has rechazado el evento',
+                    icon: 'warning',
+                    confirmButtonText: 'Continuar',
+                    confirmButtonColor: '#007bff', // Adjust color as needed
                 }).then(() => {
                     fetchData();;
-        
+
                 });
-              }
+            }
         } catch (error) {
             Swal.fire({
                 title: 'No se pudo aceptar la notificación',
@@ -85,7 +85,7 @@ const Notificaciones = () => {
                 icon: 'error',
                 confirmButtonText: 'OK',
                 confirmButtonColor: '#007bff'
-              });
+            });
         }
 
 
@@ -121,13 +121,13 @@ const Notificaciones = () => {
                                                 <span className="inline-flex items-center justify-center rounded-full bg-red-500 border-red-500 border-4"></span>
                                                 <div className='inline-flex items-center justify-center'>
                                                     <button className='text-white bg-green-700 rounded p-1 mr-1 mt-1'
-                                                        onClick= {()=>{
+                                                        onClick={() => {
                                                             handleAceptar(notificaciones.idParticipantes)
                                                         }}
-                                                            
+
                                                     >Aceptar</button>
                                                     <button className='text-white bg-red-700 rounded p-1 mt-1'
-                                                        onClick={()=>{
+                                                        onClick={() => {
                                                             handleRechazar(notificaciones.idParticipantes)
                                                         }}
                                                     >Rechazar</button>
@@ -143,7 +143,7 @@ const Notificaciones = () => {
                                     </div>
                                 </div>
                             ) : (
-                                <div className="profile_pic rounded-lg flex justify-start items-start gap-4 m-2 p-4 bg-transparent">
+                                <div className="profile_pic rounded-lg flex justify-start items-start gap-4 m-2 p-4 bg-[#f7fafd]">
                                     <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Mark-Webber" className="pl-2 w-[13%] sm:w-[8%] rounded-full" />
                                     <div className="notification-msg hover:bg-gray-200">
                                         <div className="msg-1">
@@ -158,12 +158,15 @@ const Notificaciones = () => {
                                                 <span className="inline-flex items-center justify-center rounded-full bg-red-500 border-red-500 border-4"></span>
                                             </p>
                                             <p className="text-[#939dae]"></p>
+
+
                                             <Link href={`/pages/deportes/${notificaciones.idEvento}`} className="flex justify-center">
-                                                <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-4 py-2  mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Ver perfil</button>
+                                                <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-4 py-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Ver perfil</button>
                                             </Link>
                                         </div>
                                     </div>
                                 </div>
+                         
                             )}
 
                         </section>
