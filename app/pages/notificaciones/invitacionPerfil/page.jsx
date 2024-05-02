2b699b65f2f6c4ae6e0c49b301ee51ec7d43da59
Gaston Santos <gastons525@gmail.com>
2024-05-01 21:49:35 -0300

    cagada en invitacionPerfil

--- app/pages/notificaciones/invitacionPerfil/page.jsx
+++ app/pages/notificaciones/invitacionPerfil/page.jsx
@@ -99,36 +99,36 @@ export default function InvitacionPerfilCard() {
     return (
         <div>
             <NavBar />
-            <div className="container fluid flex lg:flex hidden sm:block items-center justify-center ">
-                <div className="flex-1 min-w-96 max-w-96 shadow-md hidden sm:block mt-7">
-                    <div className="bg-gray-900 rounded-lg shadow-md p-4 text-white " style={{ backgroundImage: `url('/images/Fondo1.jpg')`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
+            <div className="container fluid flex lg:flex items-center justify-center ">
+                <div className="flex-1  min-w-96 max-w-96 shadow-md -mt-9">
+                    <div className="bg-gray-900 rounded-lg shadow-md p-9 text-white" style={{ backgroundImage: `url('/images/Fondo1.jpg')`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
 
-                        <p className="text-white-500 ml-20 pt-4">{player.deporte}<span> ⚽</span> </p>
+                        <p className="text-white-500 ml-10 pt-1">{player.deporte}<span> ⚽</span> </p>
 
                         <div className="flex items-center justify-between p-4">
 
                             <div className="flex items-center p-2">
-                                <img src={player.image} alt={player.name} className="w-12 h-12 rounded-full" />
+                                <img src={player.image} alt={player.name} className="w-10 h-10 rounded-full" />
                                 <div className="ml-4 ">
-                                    <h3 className="text-lg font-medium bg-amber-700 rounded-e-full pl-6 pr-6 pt-1 pb-1">{player.name}</h3>
+                                    <h3 className="text-sm font-medium bg-amber-700 rounded-e-full pl-6 pr-4 pt-1 pb-1">{player.name}</h3>
                                     <p className="text-white-500 mt-2">{player.position}</p>
                                 </div>
                             </div>
-                            <div className="flex items-center justify-center font-bold text-lg text-white-500 mr-8 bg-amber-700 w-12 h-12 rounded-full -mt-10">{player.media}</div>
+                            <div className="flex items-center justify-center font-bold text-lg text-white-500 mr-1 bg-amber-700 w-12 h-12 rounded-full -mt-14">{player.media}</div>
 
 
                         </div>
-                        <div className="grid grid-cols-2 gap-4 p-5 ml-28 flex items-center justify-center">
+                        <div className="grid grid-cols-2 gap-2 p-8 ml-2 flex items-center justify-center -mt-10">
                             <div className="relative mt-1 flex items-center">
                                 <Image src="/images/person.png" alt="Imagen del jugador" width={32} height={32} className="mr-2" />
                                 <span className="font-bold ">{player.edad}</span>
                             </div>
                             <div className="relative mt-1 flex items-center">
-                                <Image src="/images/altura.png" alt="Imagen del jugador" width={32} height={32} className="mr-2" />
+                                <Image src="/images/altura.png" alt="Imagen del jugador" width={32} height={32} className="mr-4" />
                                 <span className="font-bold">{player.altura} cm</span>
                             </div>
                             <div className="relative mt-1 flex items-center">
-                                <Image src="/images/pie.png" alt="Imagen del jugador" width={32} height={32} className="mr-2" />
+                                <Image src="/images/pie.png" alt="Imagen del jugador" width={32} height={32} className="mr-1" />
                                 <span className="font-bold">{player.pieHabil}</span>
                             </div>
                             <div className="relative mt-1 flex items-center">
@@ -136,7 +136,7 @@ export default function InvitacionPerfilCard() {
                                 <span className="font-bold">{player.manoHabil}</span>
                             </div>
                         </div>
-                        <div class="flex gap-0.5 flex items-center justify-center mt-2">
+                        <div class="flex gap-0.5 flex items-center justify-center -mt-2">
                             <svg className="h-8 w-8 shrink-0 fill-amber-400" viewBox="0 0 256 256">
                                 <path
                                     d="M239.2 97.4A16.4 16.4.0 00224.6 86l-59.4-4.1-22-55.5A16.4 16.4.0 00128 16h0a16.4 16.4.0 00-15.2 10.4L90.4 82.2 31.4 86A16.5 16.5.0 0016.8 97.4 16.8 16.8.0 0022 115.5l45.4 38.4L53.9 207a18.5 18.5.0 007 19.6 18 18 0 0020.1.6l46.9-29.7h.2l50.5 31.9a16.1 16.1.0 008.7 2.6 16.5 16.5.0 0015.8-20.8l-14.3-58.1L234 115.5A16.8 16.8.0 00239.2 97.4z">
@@ -164,10 +164,10 @@ export default function InvitacionPerfilCard() {
                             </svg>
                         </div>
 
-                        <div className="grid grid-cols-2 gap-4 p-4 mt-2">
-                            <div className="relative">
+                        <div className="grid grid-cols-2 gap-4 p-2 ">
+                            <div className="relative w-32 h-4">
                                 <p className="text-white-600">Velocidad</p>
-                                <span className="inline-block w-full h-4 bg-white rounded-md relative p-3">
+                                <span className="inline-block w-full bg-white rounded-md relative p-3">
                                     <span style={{ width: `${player.velocidad}%` }} className="absolute top-0 left-0 h-full bg-blue-800 rounded-md">
                                         <span className="text-sm m-2">{player.velocidad}</span>
                                     </span>
@@ -177,7 +177,7 @@ export default function InvitacionPerfilCard() {
 
                             <div className="relative">
                                 <p className="text-white-600">Disparo</p>
-                                <span className="inline-block w-full h-4 bg-white rounded-md relative p-3">
+                                <span className="inline-block w-full  bg-white rounded-md relative p-3">
                                     <span style={{ width: `${player.disparo}%` }} className="absolute top-0 left-0 h-full bg-blue-800 rounded-md">
                                         <span className="text-sm m-2">{player.disparo}</span>
                                     </span>
@@ -186,7 +186,7 @@ export default function InvitacionPerfilCard() {
 
                             <div className="relative">
                                 <p className="text-white-600">Regate</p>
-                                <span className="inline-block w-full h-4 bg-white rounded-md relative p-3">
+                                <span className="inline-block w-full bg-white rounded-md relative p-3">
                                     <span style={{ width: `${player.regate}%` }} className="absolute top-0 left-0 h-full bg-blue-800 rounded-md">
                                         <span className="text-sm m-2">{player.regate}</span>
                                     </span>
@@ -194,7 +194,7 @@ export default function InvitacionPerfilCard() {
                             </div>
                             <div className="relative">
                                 <p className="text-white-600">Fuerza</p>
-                                <span className="inline-block w-full h-4 bg-white rounded-md relative p-3">
+                                <span className="inline-block w-full  bg-white rounded-md relative p-3">
                                     <span style={{ width: `${player.fuerza}%` }} className="absolute top-0 left-0 h-full bg-blue-800 rounded-md">
                                         <span className="text-sm m-2">{player.fuerza}</span>
                                     </span>
@@ -202,7 +202,7 @@ export default function InvitacionPerfilCard() {
                             </div>
                             <div className="relative">
                                 <p className="text-white-600">Pase</p>
-                                <span className="inline-block w-full h-4 bg-white rounded-md relative p-3">
+                                <span className="inline-block w-full bg-white rounded-md relative p-3">
                                     <span style={{ width: `${player.pase}%` }} className="absolute top-0 left-0 h-full bg-blue-800 rounded-md">
                                         <span className="text-sm m-2">{player.pase}</span>
                                     </span>
@@ -210,7 +210,7 @@ export default function InvitacionPerfilCard() {
                             </div>
                             <div className="relative">
                                 <p className="text-white-600">Defensa</p>
-                                <span className="inline-block w-full h-4 bg-white rounded-md relative p-3">
+                                <span className="inline-block w-full  bg-white rounded-md relative p-3">
                                     <span style={{ width: `${player.defensa}%` }} className="absolute top-0 left-0 h-full bg-blue-800 rounded-md">
                                         <span className="text-sm m-2">{player.defensa}</span>
                                     </span>
