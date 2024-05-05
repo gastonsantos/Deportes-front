23108acc618fd2967462c5566a3df63cb203f169
Gaston Santos <gastons525@gmail.com>
2024-05-05 13:38:36 -0300

    key en participantes

--- components/misEventos/participantes.jsx
+++ components/misEventos/participantes.jsx
@@ -1,14 +1,8 @@
-"use client"
-
 import React, { useEffect, useState } from 'react';
 import { Card, Typography, CardBody } from "@material-tailwind/react";
 
-
 const Participantes = ({ duenio, evento }) => {
-
-    const { id, apellido , nombre , apodo, email } = evento;
-    const [fechaFormateada, setFechaFormateada] = useState("");
-    const [error, setError] = useState('');
+    const { id, apellido, nombre, apodo, email } = evento;
     const [formData, setFormData] = useState({
         id: id,
         nombre: nombre,
@@ -17,18 +11,11 @@ const Participantes = ({ duenio, evento }) => {
         email: email,
     });
 
-    console.log("Evento En Participantes", evento);
-
     useEffect(() => {
-        if(evento){
+        if (evento) {
             setFormData(evento);
         }
-      
     }, [evento]);
-
-
-
-
 
     return (
         <div className="w-1/2 min-w-96 bg-white rounded-xl mt-4 text-justify ml-2">
@@ -37,20 +24,14 @@ const Participantes = ({ duenio, evento }) => {
                     <Typography>
                         <p>Participantes: </p>
                         <div>{duenio}</div>
-                    {evento.map((evento) => (
-                        <div key={evento.id}>{evento.nombre} {evento.apellido}</div>
-
-                    ) )}
-
+                        {evento.map((evento) => (
+                            <div key={evento.id}>{evento.nombre} {evento.apellido}</div>
+                        ))}
                     </Typography>
                 </CardBody>
             </Card>
         </div>
+    );
+};
 
-
-
-
-    )
-}
-
-export default Participantes
+export default Participantes;
