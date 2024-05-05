3955c25d4137d4c44256537e28c3057c6023b4a5
Gaston Santos <gastons525@gmail.com>
2024-05-05 13:21:23 -0300

    key en el map de Notificaciones

--- app/pages/notificaciones/page.jsx
+++ app/pages/notificaciones/page.jsx
@@ -108,7 +108,7 @@ const Notificaciones = () => {
                         </div>
                     </section>
                     {notificaciones.map((notificaciones) => (
-                        <section className="messages m-2">
+                        <section className="messages m-2" key={notificaciones.id}>
                             {notificaciones.invitaEsDuenio ? (
                                 <div className="profile_pic rounded-lg flex justify-start items-start gap-4 m-2 p-4 bg-[#f7fafd]">
                                     <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Mark-Webber" className="pl-2 w-[13%] sm:w-[8%] rounded-full" />
