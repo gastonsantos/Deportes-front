adbdf0d7022516ddfe5651515be0787121f8451e
Gaston Santos <gastons525@gmail.com>
2024-05-05 20:06:09 -0300

    cambio en el asaide

--- components/navegation/AsideComponent.tsx
+++ components/navegation/AsideComponent.tsx
@@ -12,23 +12,13 @@ const AsideComponent: React.FC<AsideProps> = ({ openMenu }) => {
         <div className="p-4">
           <h2 className="text-2xl font-semibold">Perfil</h2>
           <ul className="mt-4 space-y-2">
-            <li><a href="/pages/perfil" className="block px-4 py-2 text-sm hover:bg-gray-700">Profile</a></li>
-            <li><a href="/pages/perfil/futbol" className="block px-4 py-2 text-sm hover:bg-gray-700">Futbol</a></li>
+            <li><a href="/pages/perfil" className="block px-4 py-2 text-sm hover:bg-gray-700">Personal</a></li>
+            <li><a href="/pages/perfil/futbol" className="block px-4 py-2 text-sm hover:bg-gray-700">Deportista</a></li>
             
           </ul>
         </div>
       </aside>
-      {/* Sidebar Mobile */}
-      <aside className={`${openMenu ? '' : 'hidden'} flex absolute h-full bg-gray-800 text-white w-3/4`}>
-        <div className="p-4">
-          <h2 className="text-2xl font-semibold">Settings</h2>
-          <ul className="mt-4 space-y-2">
-            <li><a href="#" className="block px-4 py-2 text-sm hover:bg-gray-700">Perfil</a></li>
-            <li><a href="#" className="block px-4 py-2 text-sm hover:bg-gray-700">Security</a></li>
-            <li><a href="#" className="block px-4 py-2 text-sm hover:bg-gray-700">Notifications</a></li>
-          </ul>
-        </div>
-      </aside>
+     
     </>
   );
 };
