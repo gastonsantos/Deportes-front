67f429ed9922cff91161c00f5dddb644bc4f72e1
Gaston Santos <gastons525@gmail.com>
2024-05-15 20:31:07 -0300

    fix

--- app/pages/misParticipaciones/page.jsx
+++ app/pages/misParticipaciones/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { obtenerEventoQueParticipo } from "@/services/evento/api";
 import { NavBar } from "@/components/navBar/navBar";
 import MisParticipacionesCard from "@/components/misParticipaciones/misParticipaciones";
@@ -7,16 +7,12 @@ import React, { useState, useEffect } from 'react';
 import Footer from "@/components/landing/footer";
 import NoAutorizado from "@/components/NoAutorizado/noAutorizado";
 import Cookies from 'js-cookie';
-const Misventos = () => {
 
+const Misventos = () => {
     const [evento, setEvento] = useState([]);
     const [isAuthorized, setIsAuthorized] = useState(false);
     const [checkedAuth, setCheckedAuth] = useState(false);
 
-
-    const handleDelete = (eventId) => {
-        setEvento(evento.filter(evento => evento.idEvento !== eventId));
-    };
     useEffect(() => {
         const id = Cookies.get('id');
         if (id) {
@@ -24,6 +20,30 @@ const Misventos = () => {
         }
         setCheckedAuth(true);
     }, []);
+
+    useEffect(() => {
+        if (isAuthorized) {
+            const fetchData = async () => {
+                try {
+                    const response = await obtenerEventoQueParticipo();
+                    if (response) {
+                        const data = response;
+                        if (data) {
+                            setEvento(data);
+                        }
+                        console.log("EVENTOSSS", data.dtoUsuarios);
+                    }
+                } catch (error) {
+                    console.error("Error al obtener deportes:", error);
+                }
+            };
+            fetchData();
+        }
+    }, [isAuthorized]);
+
+    const handleDelete = (eventId) => {
+        setEvento(evento.filter(evento => evento.idEvento !== eventId));
+    };
 
     if (!checkedAuth) {
         return null;
@@ -33,82 +53,45 @@ const Misventos = () => {
         return <NoAutorizado />;
     }
 
-        const fetchData = async () => {
-            try {
-                const response = await obtenerEventoQueParticipo();
-                if (response) {
-                    
-                    const data = response
-                    if(data){
-                        setEvento(data);
-                    }
-                    console.log("EVENTOSSS", data.dtoUsuarios
-                )
-                    
-                }
-            } catch (error) {
-                console.error("Error al obtener deportes:", error);
-            }
-        };
-
-    
-    useEffect(() => {
-        fetchData();
-    }, []);
-
     return (
-        <div className=" items-center justify-center">
+        <div className="items-center justify-center">
             <NavBar />
             <h2>Mis Participaciones</h2>
 
-            {
-                evento.length != 0 ? (
-
-                    <div className="grid grid-cols-1 gap-x-10 gap-y-14">
-                        {evento.map((evento) => (
-                            <MisParticipacionesCard key={evento.idEvento} evento={evento} onDelete={handleDelete} actualizar={fetchData} />
-                        ))}
-                        <div className="flex-1">
-
-
-                            
-
-                        </div>
+            {evento.length !== 0 ? (
+                <div className="grid grid-cols-1 gap-x-10 gap-y-14">
+                    {evento.map((evento) => (
+                        <MisParticipacionesCard
+                            key={evento.idEvento}
+                            evento={evento}
+                            onDelete={handleDelete}
+                            actualizar={() => {
+                                if (isAuthorized) {
+                                    fetchData();
+                                }
+                            }}
+                        />
+                    ))}
+                    <div className="flex-1"></div>
+                </div>
+            ) : (
+                <div className="flex flex-col items-center justify-center h-screen -mt-10">
+                    <div className="relative">
+                        <h2>No tienes participaciones</h2>
+                        <Image
+                            src="/images/no-eventos.jpg"
+                            alt="Image Description"
+                            width={600}
+                            height={600}
+                            className="shadow-sm rounded-xl object-cover grayscale"
+                        />
                     </div>
-
-                ) : (
-                    <>
-
-                        <div className="flex flex-col items-center justify-center h-screen -mt-10">
-
-                            <div className="relative">
-                                <h2>No tienes participaciones</h2>
-                                <Image
-                                    src="/images/no-eventos.jpg"
-                                    alt="Image Description"
-                                    width={600}
-                                    height={600}
-                                    className="shadow-sm rounded-xl object-cover grayscale"
-                                />
-
-                            </div>
-                        </div>
-
-
-
-                    </>
-
-                )}
+                </div>
+            )}
 
             <Footer />
-
         </div>
-
-
-
-
     );
-}
+};
 
-export default Misventos
-
+export default Misventos;
