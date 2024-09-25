a2628c160f038a0f28c7df0fe975db88fbc46788
gastonsantos <gastons525@gmail.com>
2024-09-25 14:44:02 -0300

    cambios menores como agrego mis datos

--- components/login/login.tsx
+++ components/login/login.tsx
@@ -4,13 +4,14 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { login } from '@/services/login/login2';
 import Image from "next/image";
-const LoginForm = () => {
 
+const LoginForm = () => {
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [showCredentials, setShowCredentials] = useState(false);
+  const [showCredentials, setShowCredentials] = useState(true);
+
   async function enviarLoginAlBackend() {
     const data = {
       email: email,
@@ -38,17 +39,20 @@ const LoginForm = () => {
             setError('Error en la petición al servidor');
         }
       } else {
-        // Mensaje de error genérico
         console.error('Error al iniciar sesión:', error.message);
         setError('Error en la petición al servidor');
       }
     }
   }
 
+  // Manejar el evento de envío del formulario
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();  // Prevenir el comportamiento por defecto del formulario
+    enviarLoginAlBackend();
+  };
 
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen">
-      
       <div className="absolute w-screen h-screen">
         <Image
           src="/images/imagen-login.jpg"
@@ -61,48 +65,67 @@ const LoginForm = () => {
       </div>
 
       <div className="absolute inset-0 flex flex-col items-center justify-center">
-      <div className="absolute top-4 left-4">
-        <button
-          onClick={() => setShowCredentials(!showCredentials)}  // Al hacer clic, se alterna el estado
-          className="bg-gray-500 hover:bg-gray-600 text-white font-semibold rounded-md py-2 px-4"
-        >
-          Demo
-        </button>
+        <div className="absolute top-4 left-4">
+          <button
+            onClick={() => setShowCredentials(!showCredentials)}  // Al hacer clic, se alterna el estado
+            className="bg-gray-500 hover:bg-gray-600 text-white font-semibold rounded-md py-2 px-4"
+          >
+            Demo
+          </button>
 
-        {/* Mostrar las credenciales si showCredentials es true */}
-        {showCredentials && (
-          <div className="mt-2 bg-black p-4 text-white border border-gray-300 rounded-md shadow-lg">
-            <p>Este es un proyecto de prueba Api rest realizado en .net</p>
-            <p>Si deseas puedes registrarte o usar el usuario de Prueba</p>
-            <p>Usuario Ejemplo</p>
-            <p><strong>Usuario:</strong> ejemploSports@mail.com</p>
-            <p><strong>Contraseña:</strong> 112233</p>
-            
-           
-          </div>
-        )}
-      </div>
+          {showCredentials && (
+            <div className="mt-2 bg-black p-4 text-white border border-gray-300 rounded-md shadow-lg">
+              <p>Este es un proyecto de prueba Api rest realizado en .net</p>
+              <p>Si deseas puedes registrarte o usar el usuario de Prueba</p>
+              <p>Usuario Ejemplo</p>
+              <p><strong>Usuario:</strong> ejemploSports@mail.com</p>
+              <p><strong>Contraseña:</strong> 112233</p>
+            </div>
+          )}
+        </div>
+
         <div className="absolute z-10 flex flex-col items-center w-96 p-4 mx-auto bg-gray-800 bg-opacity-50 rounded-lg shadow-md">
           <h2 className="text-center text-sm text-gray-400 dark:text-gray-400 p-4">
             Login
           </h2>
-          <div className="mb-5">
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 text-white">Tú email</label>
-            <input onChange={(e) => { setEmail(e.target.value) }} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-          </div>
-          <div className="mb-5">
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 text-white">Tú password</label>
-            <input onChange={(e) => { setPassword(e.target.value) }} type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-          </div>
-          <div className="mb-5">
-            <button onClick={() => { enviarLoginAlBackend() }} type="submit" className="text-white bg-purple-700 hover:bg-purple-500 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Iniciar sesión</button>
-          </div>
+          <form onSubmit={handleSubmit}>  {/* Agregar el evento onSubmit aquí */}
+            <div className="mb-5">
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 text-white">Tú email</label>
+              <input 
+                onChange={(e) => setEmail(e.target.value)} 
+                type="email" 
+                id="email" 
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                placeholder="name@flowbite.com" 
+                required 
+              />
+            </div>
+            <div className="mb-5">
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 text-white">Tú password</label>
+              <input 
+                onChange={(e) => setPassword(e.target.value)} 
+                type="password" 
+                id="password" 
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                required 
+              />
+            </div>
+            <div className="mb-5">
+              <button 
+                type="submit"  // Tipo submit para que también funcione con Enter
+                className="text-white bg-purple-700 hover:bg-purple-500 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
+              >
+                Iniciar sesión
+              </button>
+            </div>
+          </form>
 
           {error && (
-            <div className=" mb-5 text-red" >
+            <div className=" mb-5 text-red">
               <label htmlFor="" className="text-red-500">{error}</label>
             </div>
           )}
+
           <div className="mb-5 text-white">
             <a href="/pages/cambioContraseniaForm" >¿Olvidaste tu contraseña?</a>
           </div>
@@ -110,10 +133,9 @@ const LoginForm = () => {
             <a href="/pages/registro" >Crear cuenta</a>
           </div>
         </div>
-
       </div>
-
     </div>
   );
 }
+
 export { LoginForm };
